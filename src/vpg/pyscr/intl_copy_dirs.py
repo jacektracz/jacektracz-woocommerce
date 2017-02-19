8c913ad111cec7336065e0fc9fce5c9c7ded5511
jacek.tracz <jacek.tracz@hotmail.com>
2017-02-19 13:03:03 +0100

    scripts

--- src/vpg/pyscr/intl_copy_dirs.py
+++ src/vpg/pyscr/intl_copy_dirs.py
@@ -17,15 +17,46 @@ class	intl_copy_dirs:
 		#self.m_dir_root = "C:/lkd/wmtgit/v06/scate-dashboard"
 		self.m_dir_root = "C:/lkd/wmt/frontend"
 		self.m_dir_dest = "C:/lkd/wmt/frontendshort"
+		self.m_test_mode= "1"
+
+	def cpy_shorts_libs(self):
 		self.m_test_mode= "0"
+		self.short_source("")
+		self.copy_files_generic_full_libs()
+		
+	def short_source(self,tt):
+		self.m_dir_dest = "C:/lkd/wmt/frontend"
+		self.m_dir_root = "C:/lkd/wmt/frontendshort"
 		
 	def jx(self,tt):
 		return "jxpgen" + tt + "xitem"
 		
+	def copy_files_generic_full(self):
+		s_root_dst = []
+		all = 0
+		if( all == 1):
+			s_root_dst.append(self.jx("menu"))
+			s_root_dst.append(self.jx("rqsctest"))
+			s_root_dst.append(self.jx("user"))
+			s_root_dst.append(self.jx("rquserrole"))
+			self.copy_files_generic_arr(s_root_dst)
+			self.copy_from_genericL0('dao')
+		self.copy_from_generic_3("menu")
+		
+	def copy_files_generic_full_libs(self):
+		self.copy_from_genericL0('dao')
+		self.copy_from_genericL0('helpers')
+		self.copy_from_genericL0('xpgen')
+		self.copy_from_genericL0('models')
+		self.copy_from_genericL0('services')
+		self.copy_from_genericL0('helpers')
+		self.copy_fileL0("tools.js")
+		self.copy_from_generic_3(self.jx("menu"))
+		
 	def copy_files_generic(self):
 		s_root_dst = []
-		s_root_dst.append(self.jx("menu"))
-		s_root_dst.append(self.jx("rqsctest"))
+		s_root_dst.append(self.jx("rqdefview"))
+		s_root_dst.append(self.jx("rqdefcolumn"))
 		self.copy_files_generic_arr(s_root_dst)
 	
 	def copy_files_generic_arr(self, s_arr):
@@ -38,14 +69,38 @@ class	intl_copy_dirs:
 			
 	def copy_from_generic_3( self, p_path_src ):
 		try:
-			self.copy_from_generic("routes", p_path_src)
-			self.copy_from_generic("controllers", p_path_src)
-			self.copy_from_generic("templates", p_path_src)
+			self.copy_from_genericL1("routes", p_path_src)
+			self.copy_from_genericL1("controllers", p_path_src)
+			self.copy_from_genericL1("templates", p_path_src)
 			
 		except OSError as e:
 			print('file not copied. Error: %s' % e)
 			
-	def copy_from_generic( self, p_type, p_path_src):
+	def copy_from_genericL1( self, p_type, p_path_src):
+		try:
+			self.dbg_info("type:" + p_type)
+			s_root_path = self.m_dir_root + "/app/" + p_type +"/" + p_path_src + ""
+			s_dest_path = self.m_dir_dest + "/app/" + p_type +"/" + p_path_src + ""
+			s_source = s_root_path 
+			s_dest = s_dest_path 
+			#self.copy_directory(s_source, s_dest)
+			self.recursive_overwrite(s_source, s_dest, None)
+		except OSError as e:
+			print('file not copied. Error: %s' % e)
+			
+	def copy_from_genericL0( self, p_type):
+		try:
+			self.dbg_info("type:" + p_type)
+			s_root_path = self.m_dir_root + "/app/" + p_type  + ""
+			s_dest_path = self.m_dir_dest + "/app/" + p_type +	""
+			s_source = s_root_path 
+			s_dest = s_dest_path 
+			#self.copy_directory(s_source, s_dest)
+			self.recursive_overwrite(s_source, s_dest, None)
+		except OSError as e:
+			print('file not copied. Error: %s' % e)
+
+	def copy_froots( self, p_type, p_path_src):
 		try:
 			self.dbg_info("type:" + p_type)
 			s_root_path = self.m_dir_root + "/app/" + p_type +"/" + p_path_src + ""
@@ -80,8 +135,8 @@ class	intl_copy_dirs:
 	def recursive_overwrite(self, src, dest, ignore=None):
 		self.dbg_info("")
 		self.dbg_info("")			
-		self.dbg_info("copy dir:" + src )
-		self.dbg_info("to dir  :" + dest )	
+		self.dbg_info("recursive_overwrite:" + src )
+		self.dbg_info("recursive_overwrite  :" + dest )	
 		if os.path.isdir(src):
 			if not os.path.isdir(dest):
 				os.makedirs(dest)
@@ -97,14 +152,40 @@ class	intl_copy_dirs:
 										ignore)
 		else:
 			shutil.copyfile(src, dest)			
+			
+	def copy_fileL0( self, p_src):
+		try:
+			
+			s_root_path = self.m_dir_root + "/app/"
+			s_dest_path = self.m_dir_dest + "/app/"
+			s_source = s_root_path + p_src
+			s_dest = s_dest_path + p_src
+			self.copy_file(s_source, s_dest)
+		except OSError as e:
+			print('file not copied. Error: %s' % e)
+	
+	def copy_file(self, src, dest):
+		try:
+			self.dbg_info("")
+			self.dbg_info("")			
+			self.dbg_info("copy file" + src )
+			self.dbg_info("to file" + dest )
+			if self.m_test_mode == "0":
+				self.dbg_info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" )
+				self.dbg_info("real_copy_mode" )
+				shutil.copyfile(src, dest)
+				self.dbg_info("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" )
+			else:
+				self.dbg_info("test_mode" )
+			
+		except OSError as e:
+			print('file not copied. Error: %s' % e)
+			
 if __name__ == '__main__':
 	#data = intl_create_scate().copy_files_scate()
 	#data = intl_create_scate().copy_files_scate_editcreate()
-	data = intl_copy_dirs().copy_files_generic()
+	#data = intl_copy_dirs().copy_files_generic()
+	data = intl_copy_dirs().cpy_shorts_libs()
 	
 	
-# cd C:\lkd\wmtgit\w2_2\src\w2\A05_MGR_PYTHON\MGRV1\MGRPYTHON\	
-#!py C:\lkd\wmtgit\w2_2\src\w2\A05_MGR_PYTHON\MGRV1\MGRPYTHON\intl_create_scate.py			
-#python C:\lkd\wmtgit\w2_2\src\w2\A05_MGR_PYTHON\MGRV1\MGRPYTHON\intl_create_scate.py
-
-#python C:\lkd\wmt\vpg\pyscr\intl_copy_dirs.py
+#python C:\lkd\w2\gitp\src\vpg\pyscr\intl_copy_dirs.py
