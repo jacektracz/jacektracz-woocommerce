35244f5b39979c2af189d60b05cd0e3425adece5
jacek.tracz <jacek.tracz@hotmail.com>
2017-03-16 00:51:45 +0100

    completions

--- src/vpg/git/src-g.bat
+++ src/vpg/git/src-g.bat
@@ -1,6 +1,6 @@
 set cm=completions
-set b0=xph-1.0.03.16.4
-set b1=xph-1.0.03.16.5
+set b0=xph-1.0.03.16.6
+set b1=xph-1.0.03.16.7
 
 cls
 
@@ -8,30 +8,35 @@ cd C:\lkd\wmtgit\v06\scate-dashboard
 git add -A
 git commit -m %cm%
 git checkout -b %b0%
+git checkout -b %b1%
 git status 
 
 cd C:\lkd\wmtgit\v06\scate-admin-dashboard
 git add -A
 git commit -m %cm%
 git checkout -b %b0%
+git checkout -b %b1%
 git status 
 
 cd C:\lkd\wmtgit\v06\scate-backend
 git add -A
 git commit -m %cm%
 git checkout -b %b0%
+git checkout -b %b1%
 git status 
 
 cd C:\lkd\ht\apps_tools\BS_2015
 git add -A
 git commit -m %cm%
 git checkout -b %b0%
+git checkout -b %b1%
 git status 
 
 cd C:\lkd\w2\gitp
 git add -A
 git commit -m %cm%
 git checkout -b %b0%
+git checkout -b %b1%
 git status 
 
 rem C:\lkd\w2\gitp\src\vpg\git\src-g.bat
