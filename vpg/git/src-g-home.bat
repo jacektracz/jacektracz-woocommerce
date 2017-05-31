053c2ae99325fc4bd5bb262363ad588822efc3d1
jacek.tracz <jacek.tracz@hotmail.com>
2017-05-31 18:13:23 +0200

    current-2017.05.31.HM.8

---
+++ vpg/git/src-g-home.bat
@@ -0,0 +1,113 @@
+set cm=current-2017.05.31.HM.12
+
+set b0=current-2017.05.31.HM.12
+set b1=current-2017.05.31.HM.13
+
+set b2=current-2017.05.31.HM.14
+set b3=current-2017.05.31.HM.15
+
+cls
+d:
+cd D:\lkd\ht\apps_estate\lkdest
+rem dir
+rem git init
+git status
+cd D:\lkd\ht\apps_estate\lkdest
+git add -A
+git commit -m %cm%
+git checkout -b %b0%
+git checkout -b %b1%
+git checkout -b %b2%
+git status
+
+d:
+cd D:\lkd\ht\apps_joo\joo3654
+dir
+git init
+git status
+cd D:\lkd\ht\apps_joo\joo3654
+rem dir
+cd D:\lkd\ht\apps_joo\joo3654
+git add -A
+git commit -m %cm%
+git checkout -b %b0%
+git checkout -b %b1%
+git checkout -b %b2%
+git status
+
+d:
+cd D:\lkd\ht\apps_portal\lkduni\src
+dir
+cd D:\lkd\ht\apps_portal\lkduni\src
+rem dir
+cd D:\lkd\ht\apps_portal\lkduni\src
+git add -A
+git commit -m %cm%
+git checkout -b %b0%
+git checkout -b %b1%
+git checkout -b %b2%
+git status
+
+c:
+cd C:\lkd\ht\apps_tools\BS_2015
+rem dir
+cd C:\lkd\ht\apps_tools\BS_2015
+git add -A
+git commit -m %cm%
+git checkout -b %b0%
+git checkout -b %b1%
+git checkout -b %b2%
+rem git checkout -b %b3%
+git status 
+
+c:
+cd C:\lkd\wmtgit\v06-01\w2_2
+dir
+cd C:\lkd\wmtgit\v06-01\w2_2
+git add -A
+git commit -m %cm%
+git checkout -b %b0%
+git checkout -b %b1%
+rem git checkout -b %b2%
+rem git checkout -b %b3%
+git status 
+
+c:
+cd C:\lkd\wmtgit\v06\w2\gitp
+dir
+cd C:\lkd\wmtgit\v06\w2\gitp
+git add -A
+git commit -m %cm%
+git checkout -b %b0%
+git checkout -b %b1%
+git checkout -b %b2%
+git status 
+ 
+c:
+cd C:\lkd\ht\apps_tools\lkdtools_2015
+dir
+cd C:\lkd\ht\apps_tools\lkdtools_2015
+git add -A
+git commit -m %cm%
+git checkout -b %b0%
+git checkout -b %b1%
+git checkout -b %b2%
+git status   
+
+
+
+rem cd C:\lkd\wmtgit\v06\w2\gitp\
+rem C:\lkd\wmtgit\v06\w2\gitp\vpg\git\src-g-home.bat
+rem localhost:89/phpMyAdmin-v2/index.php
+rem C:\lkd\servers\httpd-2.bat
+
+rem tail -f C:\lkd\wmtgit\v06\scate-backend\src\Core\logs\log-2.txt.important.txt
+rem cd C:\lkd\wmtgit\v06\scate-dashboard
+
+rem Get-Content -Path "C:\lkd\wmtgit\v06\scate-backend\src\Core\logs\log-2.txt.important.txt" -Wait
+rem notepad "C:\lkd\wmtgit\v06\scate-backend\src\Core\logs\log-2.txt.important.txt"
+rem 733999070
+REM  00-1F-D0-27-CD-DF -2 
+
+REM 00-1F-D0-27-CD-CF  -1 TO 00-1F-D0-27-CD-DF  001FD027CDAF jem0zOyqet
+
