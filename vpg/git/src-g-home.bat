fc9c7aca6576583852409a42ac705e4da1c8f95b
jacek.tracz <jacek.tracz@hotmail.com>
2017-05-31 19:16:39 +0200

    current-2017.05.31.HM.24

--- vpg/git/src-g-home.bat
+++ vpg/git/src-g-home.bat
@@ -1,10 +1,10 @@
-set cm=current-2017.05.31.HM.20
+set cm=current-2017.05.31.HM.24
 
-set b0=current-2017.05.31.HM.20
-set b1=current-2017.05.31.HM.21
+set b0=current-2017.05.31.HM.24
+set b1=current-2017.05.31.HM.25
 
-set b2=current-2017.05.31.HM.22
-set b3=current-2017.05.31.HM.23
+set b2=current-2017.05.31.HM.26
+set b3=current-2017.05.31.HM.27
 
 cls
 d:
