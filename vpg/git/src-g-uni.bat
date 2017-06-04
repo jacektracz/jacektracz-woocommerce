a64a72a77b8f2e6055f0365e5588b495f542a65d
jacek.tracz <jacek.tracz@hotmail.com>
2017-06-04 18:38:16 +0200

    current-2017.06.04.HM.30

--- vpg/git/src-g-uni.bat
+++ vpg/git/src-g-uni.bat
@@ -1,10 +1,10 @@
-set cm=current-2017.06.04.HM.26
+set cm=current-2017.06.04.HM.30
 
-set b0=current-2017.06.04.HM.26
-set b1=current-2017.06.04.HM.27
+set b0=current-2017.06.04.HM.30
+set b1=current-2017.06.04.HM.31
 
-set b2=current-2017.06.04.HM.28
-set b3=current-2017.06.04.HM.29
+set b2=current-2017.06.04.HM.32
+set b3=current-2017.06.04.HM.33
 
 cls
 d:
