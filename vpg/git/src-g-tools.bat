5d1ac659de18af8ad9e30fe6b4c1762b70553a17
jacek.tracz <jacek.tracz@hotmail.com>
2017-06-19 06:58:27 +0200

    current-2017.06.19.HM.0.berta-gen-01

--- vpg/git/src-g-tools.bat
+++ vpg/git/src-g-tools.bat
@@ -2,9 +2,9 @@
 rem cd C:\lkd\wmtgit\v06\w2\gitp\vpg\git
 rem C:\lkd\wmtgit\v06\w2\gitp\vpg\git\src-g-tools.bat
 
-set ii0=4
+set ii0=0
 
-set dt=18
+set dt=19
 set comment=berta-gen-01
 set smode=HM
 
