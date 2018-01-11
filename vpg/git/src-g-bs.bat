8b345b99b8a7b243b85e7fb8278246239d01fce5
jacek.tracz <jacek.tracz@hotmail.com>
2018-01-11 21:09:05 +0100

    curr-2018.01.11.WI.0.tools-start

--- vpg/git/src-g-bs.bat
+++ vpg/git/src-g-bs.bat
@@ -2,9 +2,9 @@ rem cd C:\lkd\wmtgit\v06\w2\gitp\vpg\git\
 rem C:\lkd\wmtgit\v06\w2\gitp\vpg\git\src-g-bs.bat
 
 
-set ii0=50
-set dt=31
-set dtm=12
+set ii0=0
+set dt=11
+set dtm=01
 set comment=tools-start
 set /a "ii1=%ii0%+1"
 set /a "ii2=%ii0%+2"
@@ -14,13 +14,13 @@ echo "ROOT_OF_CALC_START"
 echo %ii1%
 echo "ROOT_OF_CALC_END"
 
-set cm=curr-2017.%dtm%.%dt%.WI.%ii0%.%comment%
+set cm=curr-2018.%dtm%.%dt%.WI.%ii0%.%comment%
 
 
-set b0=curr-2017.%dtm%.%dt%.WI.%ii0%.%comment%
-set b1=curr-2017.%dtm%.%dt%.WI.%ii1%.%comment%
-set b2=curr-2017.%dtm%.%dt%.WI.%ii2%.%comment%
-set b3=curr-2017.%dtm%.%dt%.WI.%ii3%.%comment%
+set b0=curr-2018.%dtm%.%dt%.WI.%ii0%.%comment%
+set b1=curr-2018.%dtm%.%dt%.WI.%ii1%.%comment%
+set b2=curr-2018.%dtm%.%dt%.WI.%ii2%.%comment%
+set b3=curr-2018.%dtm%.%dt%.WI.%ii3%.%comment%
 
 
 rem set b2=current-2017.04.25.26
@@ -69,27 +69,6 @@ rem git checkout -b %b3%
 dir
 git status 
 
-cd C:\lkd\ht\apps_jhipster\eval-1\eval-a2
-git checkout -b %b0%
-git add -A
-git commit -m %cm%
-git checkout -b %b1%
-git checkout -b %b2%
-rem git checkout -b %b2%
-rem git checkout -b %b3%
-dir
-git status 
-
-cd C:\lkd\ht\apps_portal\lkduni
-git checkout -b %b0%
-git add -A
-git commit -m %cm%
-git checkout -b %b1%
-git checkout -b %b2%
-rem git checkout -b %b2%
-rem git checkout -b %b3%
-dir
-git status 
 
 d:
 cd d:\lkd\ht\apps_portal\lkduni
