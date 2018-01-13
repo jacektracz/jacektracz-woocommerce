e9f56093c44eed0b2c1af4add9a8684e7987639f
jacek.tracz <jacek.tracz@hotmail.com>
2018-01-13 08:11:24 +0100

    curr-2018.01.13.WI.0.tools-start

--- vpg/git/src-g-bs.bat
+++ vpg/git/src-g-bs.bat
@@ -2,8 +2,8 @@ rem cd C:\lkd\wmtgit\v06\w2\gitp\vpg\git\
 rem C:\lkd\wmtgit\v06\w2\gitp\vpg\git\src-g-bs.bat
 rem mysql <DB_NAME> -h <DB_HOST> -u <DB_USER> -p<DB_PASSWORD> < <PLIK>
 
-set ii0=4
-set dt=12
+set ii0=0
+set dt=13
 set dtm=01
 set comment=tools-start
 set /a "ii1=%ii0%+1"
