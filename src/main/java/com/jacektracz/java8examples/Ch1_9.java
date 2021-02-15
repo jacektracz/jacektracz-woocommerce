1e9b25749ed549019cef4bd1c990465c291193aa
jacek_tracz@wp.pl <jacek_tracz@wp.pl>
2021-02-15 15:06:23 +0100

    2021.02.15.15.00.0

--- src/main/java/com/jacektracz/java8examples/Ch1_9.java
+++
@@ -1,9 +0,0 @@
-package com.jacektracz.java8examples;
-public class Ch1_9 {
-    public static void main(String[] args) {
-        var s = new String("my string");
-        var t = new String("my string");
-        System.out.printf("new String(\"my string\").equals(new String(\"my string\")) should be true is %b\n", s.equals(t));
-        System.out.printf("new String(\"my string\") == new String(\"my string\") should be false is %b\n", s == t);
-    }
-}
--- src/main/java/com/jacektracz/java8examples/Ch7_6.java
+++
@@ -1,9 +0,0 @@
-package com.jacektracz.java8examples;
-import java.util.Map;
-import java.util.Set;
-
-public class Ch7_6 {
-    public static <AS extends Set<Integer>> void someMethod(Map<String, AS> input) {
-        // testing compilation only
-    }
-}
